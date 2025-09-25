4a92f4475c6ee004e88bacf4b1be3c88d5415065
Cedric <crubbertire@rogers.com>
2025-09-25 14:37:04 -0400

    Added several exercises, Check last exercise for added functionality

--- app.js
+++ app.js
@@ -34,5 +34,130 @@ Solve Exercise 3 here:
 
 game.difficulty= "Easy";
 
+
+/*
+Exercise 4
+1. Select a starter Pokémon from the `pokemon` array. Remember, a starter Pokémon's `starter` property is true.
+2. Add this Pokémon to the `game.party` array. Which array method will you use to add them?
+
+
+Solve Exercise 4 here:
+*/
+
+game.party.push(pokemon[6]);
+
+
+/*
+Exercise 5
+1. Choose three more Pokémon from the `pokemon` array and add them to your party.
+2. Consider different attributes like 'type' or 'HP' for your selection. Which array method will you use to add them?
+
+
+Solve Exercise 5 here:
+*/
+//                      Magikarp      Karfetch'd   Mr. Mime
+game.party.splice(1, 0, pokemon[128], pokemon[82], pokemon[121]);
+// I given the chance, I would have done 3 Magikarps, but c'est la vie, eh?
+
+/*
+Exercise 6
+1. Set the `completed` property to true for gyms with a difficulty below 3.
+2. Think about how you'd loop through the `gyms` array to check and update the `completed` property.
+
+
+Solve Exercise 6 here:
+*/
+
+for (let i = 0; i < 3; i++) {
+  game.gyms[i].completed = true;
+}
+
+
+/*
+Exercise 7
+1. Evolve the starter Pokémon you added to your party earlier. Each starter Pokémon evolves into a specific one.
+2. How would you replace the current starter Pokémon in your party with its evolved form?
+
+Hint: 
+  - Pokemon 1: Bulbasaur evolves into Pokemon 2: Ivysaur
+  - Pokemon 4: Charmander evolves into Pokemon 5: Charmeleon
+  - Pokemon 7: Squirtle evolves into Pokemon 8: Wartortle
+  - Pokemon 25: Pikachu evolves into Pokemon 26: Raichu
+
+More Hints: The existing starter Pokemon will be *replaced* in your party with the Pokemon it evolved into. When working with an array of objects, the splice() array method is ideal for replacing one element with another. 
+
+Solve Exercise 7 here:
+*/
+
+game.party.splice(0,1,pokemon[game.party[0].number])
+
+
+/*
+Exercise 8
+1. Print the name of each Pokémon in your party.
+2. Consider using a loop or an array method to access each Pokémon's name.
+
+Solve Exercise 8 here:
+*/
+
+
+game.partyLog = function() {
+  const partyName = []
+  console.log("\nCurrent party: ")
+  this.party.forEach((poke) => {
+    partyName.push(poke.name)
+  });
+  console.log(partyName.join(", "))
+}
+
+game.partyLog();
+
+
+
+/*
+Exercise 9
+1. Can you print out all the starter Pokémon from the `pokemon` array?
+2. Think about how you can identify a starter Pokémon and then log their names.
+
+
+Solve Exercise 9 here:
+*/
+
+console.log("\nStarter Pokemon: ")
+pokemon.forEach((poke) => {
+  if (poke.starter) {
+    console.log(poke);
+  }
+});
+
+
+
+/*
+Exercise 10
+Create a method called `catchPokemon` and add it to the `game` object. You should not need to edit the original game object directly. This method should:
+  - Accept an object as a parameter called `pokemonObj`
+  - Add the `pokemonObj` to the `game.party` array.
+  - not return anything
+
+After writing this method, call it and pass in a Pokemon object of your choice from the `pokemon` data to catch it.
+
+Solve Exercise 10 here:
+*/
+
+game.catchPokemon = function (pokemonObj) {
+  if (game.party.length === 6) { // A party can only have 6 pokemon at a time!
+    // 1st, check if the pc object exists
+    // If yes, push a pokemon to the array
+    // If no, add a property to the game obj called pc.
+    // Store that extra pokemon in the pc.
+    ('pc' in game) ? game.pc.push(pokemonObj) : game.pc = [pokemonObj] ; 
+  } else { game.party.push(pokemonObj) }
+}
+
+game.catchPokemon(pokemon[24])
+
+
+
 // console.dir(pokemon, { maxArrayLength: null })
-console.log(game)
+console.log(game.party)
+console.log(game.pc)
